01b13112c5da7e06b0da5ba549d39f23bda3e861
YunSuHee <suheeyun97@gmail.com>
2022-08-07 16:28:36 +0900

    0807

--- README.md
+++ README.md
@@ -39,6 +39,9 @@
 - list.sort(Comparator.naturalOrder()) //오름차순
 - list.sort(Comparator.reverseOrder()) //내림차순
 
+**list 내부 다 지우기**
+- ArrayList.clear()
+
 ### 특정값 포함되어 있는지 확인(Contains)
 ```
 Arrays.asList(배열).contains(값)
--- src/Baekjoon/수이어가기_2635.java
+++ src/Baekjoon/수이어가기_2635.java
@@ -11,38 +11,32 @@ public class 수이어가기_2635 {
 		int input = sc.nextInt();
 		// 최종 출력될 리스트;
 		ArrayList<Integer> result = new ArrayList<>();
-		//ArrayList<Integer> temp = new ArrayList<>();
-		
-		int maxNum =0;
+
+		int maxNum = 0;
 		for (int i = input; i > 0; i--) {
 			ArrayList<Integer> temp = new ArrayList<>();
 			temp.add(input);
 			temp.add(i);
 
-			for(int j =0; j<temp.size()-1;j++) {
-				//음의 정수가 아닐때니까 0도 포함
-				if(temp.get(j)-temp.get(j+1)>=0) {
-					temp.add(temp.get(j)-temp.get(j+1));
+			for (int j = 0; j < temp.size() - 1; j++) {
+				// 음의 정수가 아닐때니까 0도 포함
+				if (temp.get(j) - temp.get(j + 1) >= 0) {
+					temp.add(temp.get(j) - temp.get(j + 1));
 				}
 			}
-			
-			if(temp.size()>maxNum) {
+
+			if (temp.size() > maxNum) {
 				maxNum = temp.size();
-				//temp 리스트가 for문 밖에 있을 때 result 리스트로 깊은 복사하기 // 기존 리스트 초기화하고
-//				result.clear();
-//				result = (ArrayList<Integer>) temp.clone();
-				
-				//temp 리스트가 for문 안에 있을때엔 얕은 복사로 리스트 복사가능
+
+				// temp 리스트가 for문 안에 있을때엔 얕은 복사로 리스트 복사가능
 				result = temp;
-				
-				
+
 			}
 
-			
 		}
 		System.out.println(maxNum);
-		for(int i: result) {
-			System.out.print(i+" ");
+		for (int i : result) {
+			System.out.print(i + " ");
 		}
 
 	}
