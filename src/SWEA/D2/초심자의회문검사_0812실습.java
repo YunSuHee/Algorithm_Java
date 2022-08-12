601aa38dc754191c72eddd5bd5fd48ba17e4637b
SuHee <97869193+YunSuHee@users.noreply.github.com>
2022-08-12 17:49:44 +0900

    Add files via upload

---
+++ src/SWEA/D2/초심자의회문검사_0812실습.java
@@ -0,0 +1,26 @@
+package swea;
+
+import java.util.Scanner;
+
+public class 초심자의회문검사_0812실습 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+
+		for (int i = 1; i <= tc; i++) {
+			String input = sc.next();
+			StringBuffer sb = new StringBuffer(input);
+
+			String reverse = sb.reverse().toString();
+
+			if (input.equals(reverse)) {
+				System.out.println("#" + i + " " + 1);
+			} else {
+				System.out.println("#" + i + " " + 0);
+			}
+		}
+
+	}
+
+}
