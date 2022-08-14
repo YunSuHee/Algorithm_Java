2db64622b2bbf7405a11035bcd473f6895f68312
SuHee <97869193+YunSuHee@users.noreply.github.com>
2022-08-14 18:57:36 +0900

    [Silver IV] Title: 색종이 - 2, Time: 224 ms, Memory: 17868 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
@@ -7,7 +7,7 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[][] paper = new int[100][100];
+		int[][] paper = new int[101][101];
 
 		for (int i = 0; i < n; i++) { // 색종이 부분 1로 만들기
 			int x = sc.nextInt();
@@ -24,15 +24,13 @@ public class Main {
 		int[] dx = { 1, -1, 0, 0 };
 		int[] dy = { 0, 0, -1, 1 };
 
-		for (int i = 0; i < 100; i++) {
-			for (int j = 0; j < 100; j++) {
+		for (int i = 0; i < 101; i++) {
+			for (int j = 0; j < 101; j++) {
 				if (paper[i][j] == 1) {// 사방탐색 하여 0 개수 체크
 					for (int k = 0; k < 4; k++) {
 						int x = i + dx[k];
 						int y = j + dy[k];
-						if (x > 99 || y > 99) { // 처음부터 자연수가 들어오기 때문에 x,y가 음수일 경우는 없다
-							cnt++;
-						}else if (paper[x][y] == 0) {
+						if (paper[x][y] == 0) {
 							cnt++;
 						}
 					}
