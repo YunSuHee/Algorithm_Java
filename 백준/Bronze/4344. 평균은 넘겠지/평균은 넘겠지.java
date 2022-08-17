b7c0f72fcda5436114fb8f016fc2241e354f70a0
SuHee <97869193+YunSuHee@users.noreply.github.com>
2022-08-17 19:52:49 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 420 ms, Memory: 33884 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int c = sc.nextInt();
+		for (int t = 0; t < c; t++) {
+			int n = sc.nextInt();
+			int[] student = new int[n];
+			int total = 0;
+			for (int i = 0; i < n; i++) {
+				int value = sc.nextInt();
+				student[i] = value;
+				total += value;
+			}
+			int avg = total / n;
+			int cnt = 0;
+			for (int i = 0; i < n; i++) {
+				if (student[i] > avg) {
+					cnt++;
+				}
+			}
+
+			double stu = cnt/(double)n*100 ;
+			System.out.printf("%.3f%s\n",stu,"%");
+
+
+		}
+
+	}
+
+}
