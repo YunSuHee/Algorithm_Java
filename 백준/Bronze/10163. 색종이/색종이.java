602f182aa3b52141f413a729d5a548dea28efa9c
SuHee <97869193+YunSuHee@users.noreply.github.com>
2022-08-30 23:54:42 +0900

    [Bronze I] Title: 색종이, Time: 520 ms, Memory: 24880 KB -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		final int MAX_NUMBER = 1001;
+		int[][] paper = new int[MAX_NUMBER][MAX_NUMBER];
+
+		for (int T = 1; T <= N; T++) {
+
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			int width = sc.nextInt();
+			int height = sc.nextInt();
+
+			for (int i = x; i < x + width; i++) {
+				for (int j = y; j < y + height; j++) {
+					paper[i][j] = T;
+				}
+			}
+		}
+		//System.out.println(Arrays.deepToString(paper));
+
+
+		for(int t=1; t<=N; t++) {
+			int cnt = 0;
+			for(int i=0; i<MAX_NUMBER; i++) {
+				for (int j=0; j<MAX_NUMBER; j++) {
+					if(paper[i][j] == t) {
+						cnt++;
+					}
+				}
+			}
+			System.out.println(cnt);
+		}
+
+	}
+
+}
