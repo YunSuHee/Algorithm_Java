74b79837142b725e93141ad66736685fa7610dfb
SuHee <97869193+YunSuHee@users.noreply.github.com>
2022-09-21 23:54:32 +0900

    [Silver III] Title: N과 M (3), Time: 600 ms, Memory: 271556 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+//중복순열
+
+public class Main {
+	static int n;
+	static int m;
+	static int[] result;
+	static int[] num;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		result = new int[m];
+		num = new int[n];
+		for(int i=1;i<=n;i++) {
+			num[i-1] = i;
+		}
+		
+		rperm(0);
+		System.out.println(sb);
+
+	}
+	static void rperm(int idx) {
+		if(idx == m) {
+			for(int i: result) {
+				sb.append(i+" ");
+				//System.out.print(i+" ");-> 시간 초과 남
+			}
+			//System.out.println();
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=0;i<n;i++) {
+			result[idx] = num[i];
+			rperm(idx+1);
+		}
+	}
+
+}
