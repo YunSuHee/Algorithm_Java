f55a3184a773e6d4f2f2be8a99a9960f27b5e2cc
SuHee <97869193+YunSuHee@users.noreply.github.com>
2022-09-22 20:22:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 132 ms, Memory: 13100 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,10 +1,10 @@
 import java.util.*;
-
+//dfs
 public class Main {
 	static int n;
 	static int[][] apart;
 	static boolean[][] check;
-
+	static int answer = 1;
 	static int[] dx = { 0, 0, -1, 1 };
 	static int[] dy = { 1, -1, 0, 0 };
 
@@ -29,7 +29,9 @@ public class Main {
 			for (int j = 0; j < n; j++) {
 				if (apart[i][j] == 1 && !check[i][j]) {
 					check[i][j] = true;
-					result.add(bfs(i, j));
+					dfs(i, j);
+					result.add(answer);
+					answer = 1;
 				}
 			}
 		}
@@ -42,31 +44,22 @@ public class Main {
 
 	}
 
-	static int bfs(int x, int y) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[] { x, y });
-		int result = 1;
+	static void dfs(int x, int y) {
+		
+		for (int i = 0; i < 4; i++) {
+			int nextx = x + dx[i];
+			int nexty = y + dy[i];
 
-		while (!queue.isEmpty()) {
-			int[] now = queue.poll();
-			int nowx = now[0];
-			int nowy = now[1];
-
-			for (int i = 0; i < 4; i++) {
-				int nextx = nowx + dx[i];
-				int nexty = nowy + dy[i];
-
-				if (nextx >= 0 && nextx < n && nexty >= 0 && nexty < n && apart[nextx][nexty] == 1) {
-					if (check[nextx][nexty])
-						continue;
-
-					check[nextx][nexty] = true;
-					queue.add(new int[] { nextx, nexty });
-					result++;
-				}
+			if (nextx >= 0 && nextx < n && nexty >= 0 && nexty < n && apart[nextx][nexty] == 1) {
+				if (check[nextx][nexty])
+					continue;
+				check[nextx][nexty] = true;
+				answer++;
+				dfs(nextx,nexty);
 			}
 		}
-		return result;
+
+	
 	}
 
 }
