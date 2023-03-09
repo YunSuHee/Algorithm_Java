0f0ee1462635069f979662b511866718a67a8b94
SuHee <97869193+YunSuHee@users.noreply.github.com>
2023-03-09 13:53:26 +0900

    [Silver I] Title: 점프, Time: 148 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+       
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         int[][] jump = new int[n][n];
+
+        //dp
+        // 경로의 개수는 2^63-1보다 작거나 같으니 long타입
         long[][] dp = new long[n][n];
 
         for(int i=0; i<n; i++){
@@ -14,13 +18,22 @@ public class Main {
                 jump[i][j] = sc.nextInt();
             }
         }
+        //출발지 값 지정
         dp[0][0] = 1;
+
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
+                //점프할 수 있는 거리
                 int next = jump[i][j];
+
+                //JUMP 했을때 도착지인 경우
                 if(next == 0) break;
+
+                //아래로 내려가는 경우
                 if(i+next<n) dp[i+next][j] += dp[i][j];
+                //오른쪽으로 내려가는 경우
                 if(j+next<n) dp[i][j+next] += dp[i][j];
+
             }
         }
         System.out.println(dp[n-1][n-1]);
