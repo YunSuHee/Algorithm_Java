aafcba24233e93720bb246149706de30633a9d70
SuHee <97869193+YunSuHee@users.noreply.github.com>
2023-09-12 16:28:36 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,39 +1,32 @@
 import java.util.*;
 class Solution {
-    static boolean[][] visit;
-    public int solution(int n, int[][] computers) {
-        visit = new boolean[n][n];
-        int answer = 0;
-        for(int i=0; i<n; i++){    
-            Queue<int[]> queue = new LinkedList<>();
-            for(int j=0; j<n; j++){
-                if(!visit[i][j] && computers[i][j]==1){
-                    visit[i][j] = true;
-                    visit[j][i] = true;
-                    queue.add(new int[]{i,j});
-                }
-            }
-            if(!queue.isEmpty()){
-              answer++;
-              bfs(n,computers,queue);  
-            }          
 
+    public int solution(int n, int[][] computers) {
+        boolean[] visit = new boolean[n];
+        int answer = 0;
+      
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<n; i++){
+            if(!visit[i]){
+                answer++;
+                visit[i] = true;
+                queue.add(i);
+                while(!queue.isEmpty()){
+                    int row = queue.poll();
+
+                    for(int j=0; j<n; j++){
+                        if(!visit[j]&&computers[row][j]==1){
+                            visit[j] = true;
+                            queue.add(j);
+                        }
+                    }
+                }
+                
+            }
         }
+
         return answer;
     }
-    static void bfs(int n,int[][] computers,Queue<int[]> queue){      
-        while(!queue.isEmpty()){
-            int[] now = queue.poll();
-            int row = now[1];
-            for(int j=0; j<n; j++){
-                if(!visit[row][j]&&computers[row][j]==1){
-                    visit[row][j] = true;
-                    visit[j][row] = true;
-                    queue.add(new int[]{row,j});
-                }
-            }
-        }
-            
-   }
+
 
 }
