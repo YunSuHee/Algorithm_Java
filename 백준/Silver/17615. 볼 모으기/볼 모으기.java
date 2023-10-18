f6e01524038fe7b77642de19b8ff97d0968d755e
SuHee <97869193+YunSuHee@users.noreply.github.com>
2023-10-18 16:49:11 +0900

    [Silver I] Title: 볼 모으기, Time: 172 ms, Memory: 17728 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.java
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -1,55 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String balls = br.readLine();
+        int red = 0;
+        int blue = 0;
+        for(int i=0; i<n; i++){
+            if(balls.charAt(i)=='R'){
+                red++;
+            }else{
+                blue++;
+            }
+        }
+
         int answer = Integer.MAX_VALUE;
         //1. 왼쪽으로 빨간 공 옮기는 경우
         int cnt = 0;
-        boolean isJump = false;
         for(int i=0; i<n; i++){
-            if(balls.charAt(i)=='R' && isJump){
+            if(balls.charAt(i)=='R'){
                 cnt++;
+            }else{
+                break;
             }
-            if(balls.charAt(i)=='B') isJump = true;
         }
-        answer = Math.min(cnt,answer);
+        answer = Math.min(red-cnt,answer);
         //2. 왼쪽으로 파란 공 옮기는 경우
         cnt = 0;
-        isJump = false;
         for(int i=0; i<n; i++){
-            if(balls.charAt(i)=='B' && isJump){
+            if(balls.charAt(i)=='B'){
                 cnt++;
+            }else{
+                break;
             }
-            if(balls.charAt(i)=='R') isJump = true;
+
         }
-        answer = Math.min(answer,cnt);
+        answer = Math.min(answer,blue-cnt);
+
         //3. 오른쪽으로 빨간 공 옮기는 경우
         cnt = 0;
-        isJump = false;
         for(int i=n-1; i>=0; i--){
-            if(balls.charAt(i)=='R'&&isJump){
+            if(balls.charAt(i)=='R'){
                 cnt++;
+            }else{
+                break;
             }
-            if(balls.charAt(i)=='B') isJump = true;
         }
-        answer = Math.min(answer,cnt);
-
+        answer = Math.min(answer,red-cnt);
         //4. 오른쪽으로 파란 공 옮기는 경우
         cnt = 0;
-        isJump = false;
         for(int i=n-1; i>=0; i--){
-            if(balls.charAt(i)=='B' && isJump){
+            if(balls.charAt(i)=='B'){
                 cnt++;
+            }else{
+                break;
             }
-            if(balls.charAt(i)=='R') isJump = true;
         }
-        answer = Math.min(answer,cnt);
+        answer = Math.min(answer,blue-cnt);
         System.out.println(answer);
 
     }
